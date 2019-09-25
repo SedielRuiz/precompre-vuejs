72f978b2c582c76ce629e63ae95fb15df1b2206b
Sediel Ruiz <Sediel@gmail.com>
2019-09-25 09:21:07 -0500

    fix

--- src/components/Customers/customer-list.vue
+++ src/components/Customers/customer-list.vue
@@ -68,7 +68,7 @@
                       <i class="material-icons">check_circle_outline</i>
                     </div>
                     <div v-else>
-                      <v-checkbox align-center value @click="verifyNumberCode(c)"></v-checkbox>
+                      <v-checkbox align-center value @click="verifyNumberCode(c)" style="margin-top: -12px;"></v-checkbox>
                       <!--v-btn color="success" @click="verifyNumberCode(c)">Validar</v-btn-->
                     </div>
                   </v-flex>
