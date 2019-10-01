a0337c3b612fba5876f7793c52304eddb38afe25
Sediel Ruiz <Sediel@gmail.com>
2019-10-01 09:09:23 -0500

    fix

--- src/components/Customers/customer-list.vue
+++ src/components/Customers/customer-list.vue
@@ -146,6 +146,7 @@ import Vue from 'vue'
         },
         places(val){
           if(val){
+            this.delivery_places.push({ text:"Vacio", value:"" });
             for(var s = 0; s < val.length; s++){
               this.delivery_places.push({ text:val[s].name, value:val[s]._id });
             }
