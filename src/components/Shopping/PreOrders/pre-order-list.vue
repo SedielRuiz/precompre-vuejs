4502e558ec67b7c56f625f97fd07dcaed638c041
Sediel Ruiz <Sediel@gmail.com>
2019-10-15 12:04:05 -0500

    fix lista pre orders

--- src/components/Shopping/PreOrders/pre-order-list.vue
+++ src/components/Shopping/PreOrders/pre-order-list.vue
@@ -5,21 +5,22 @@
           <h1>Pre ordenes</h1>
         </v-flex>
         <v-flex xs12 sm12 md10>
-            <v-btn color="success" @click="redirect()">Nueva pre orden</v-btn>
+            <v-btn color="success" @click="redirect(true)">Nueva pre orden</v-btn>
             <v-btn color="success"><v-icon medium @click="search({page_size:10})"tooltip="Detalle">refresh</v-icon></v-btn>
         </v-flex>
     </v-layout>
     <hr><br>
     <v-data-table
         :headers="headers"
-        :items="rows"
+        :items="preOrders"
         hide-actions disable-initial-sort
         class="elevation-1">
         <template v-slot:items="props">
-          <td>{{ props.item.delivery_date ? props.item.delivery_date.split("T")[0] : "" }}</td>
-          <td>{{ props.item.delivery_place ? props.item.delivery_place.name : "" }} {{ props.item.delivery_place ? props.item.delivery_place.unit_name : "" }}</td>
-          <td>{{ props.item.state }}</td>
-          <td><v-btn color="primary" @click="redirect(false, props.item._id)">Detalle</v-btn></td>
+          <td>{{ props.item.name }}</td>
+          <td>{{ props.item.date }}</td>
+          <td>{{ props.item.delivery}}</td>
+          <td>{{ props.item.customer }}</td>
+          <td><v-btn color="primary" @click="redirect(false, props.item.customer_id)">Detalle</v-btn></td>
         </template>
     </v-data-table>
     <pagination @search="search" :total_pages="total_pages" :total_items="total_items" :page_size="page_size"></pagination>
@@ -38,11 +39,34 @@
     data () {
       return {
         headers: [
-            {text:"Fecha", value:"delivery_date"},
+            {text:"Nombre", value:"name"},
+            {text:"Fecha creación", value:"delivery_date"},
             {text:"Lugar de estrega", value:"delivery_place"},
-            {text:"Estado", value:"state"},
+            {text:"Cliente", value:"customer"},
             {text:"Acciones", value:"actons"}
-        ]
+        ],
+        preOrders:[],
+      }
+    },
+    watch:{
+      rows(val){
+        if(val){
+          var lst = [];
+          var dt = "";
+          var d;
+          for(var s = 0; s < val.length; s++){
+            d = val[s].pre_orders[0].created_at.split("T")[0].split("-");
+            dt = d[2]+"/"+d[1]+"/"+d[0];
+            lst.push({
+              delivery:val[s].pre_orders[0].delivery_place.place_name+" "+val[s].pre_orders[0].delivery_place.cluster_title+" - "+val[s].pre_orders[0].delivery_place.unit_u,
+              customer:val[s].pre_orders[0].customer.name +" "+val[s].pre_orders[0].customer.last_name,
+              customer_id:val[s].pre_orders[0].customer._id,
+              date:dt,
+              name:val[s].pre_orders[0].group_name,
+            });
+          }
+          this.preOrders = lst;
+        }
       }
     },
     mounted () {
@@ -56,8 +80,12 @@
       search(pagination){
         this.fetchPreOrders(pagination);
       },
-      redirect(){
-        this.$router.push('/preOrder')
+      redirect(page, id){
+        if(!page){
+          this.$router.push('/preOrder/'+id)
+        }else{
+          this.$router.push('/preOrder');
+        }
       }
     },
     computed:{
