9f4aa279a5ab84f0ae0a3a85770c31d259369255
Sediel Ruiz <Sediel@gmail.com>
2019-10-17 16:07:34 -0500

    _fix version

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -302,7 +302,7 @@
       <router-view></router-view>
     </v-content>
     <v-footer color="indigo" app>
-      <span class="white--text">&copy; v0.0.52</span>
+      <span class="white--text">&copy; v0.0.53</span>
     </v-footer>
   </v-app>
 </template>
